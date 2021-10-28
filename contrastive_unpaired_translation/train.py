5d227c6af176bba5d42dca425f3ca37870d101ed
kuailefeifei <58092010+kuailefeifei@users.noreply.github.com>
2021-10-28 21:00:39 +0800

    fix

--- contrastive_unpaired_translation/train.py
+++ contrastive_unpaired_translation/train.py
@@ -4,9 +4,11 @@ from options.train_options import TrainOptions
 from data import create_dataset
 from models import create_model
 # from util.visualizer import Visualizer
+from tensorboardX import SummaryWriter
 
 
 if __name__ == '__main__':
+    writer = SummaryWriter('writer')
     opt = TrainOptions().parse()   # get training options
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     dataset_size = len(dataset)    # get the number of images in the dataset.
@@ -56,6 +58,9 @@ if __name__ == '__main__':
 
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
+                for k, v in losses.items():
+                    writer.add_scalar(k, v, total_iters)
+
                 # visualizer.print_current_losses(epoch, epoch_iter, losses, optimize_time, t_data)
                 # if opt.display_id is None or opt.display_id > 0:
                 #     visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
